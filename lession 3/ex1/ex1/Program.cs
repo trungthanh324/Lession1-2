804bb7598bef0c364e972a1d6e6d5c6cfeba1415
trungthanh324 <136559582+trungthanh324@users.noreply.github.com>
2023-12-07 17:27:11 +0700

    add Lession 3

---
+++ lession 3/ex1/ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Query using Query Syntax");
+            //Data Source
+            List<int> integerList = new List<int>()
+{
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+            //LINQ Query using Query Syntax
+            var QuerySyntax = from obj in integerList
+                              where obj > 5
+                              select obj;
+            //Execution
+            foreach (var item in QuerySyntax)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nLINQ Query using Method Syntax");
+            List<int> integerList1 = new List<int>()
+{
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+            //LINQ Query using Method Syntax
+            var MethodSyntax = integerList1.Where(obj => obj > 5).ToList();
+            //Execution
+            foreach (var item in MethodSyntax)
+            {
+                Console.Write(item + " ");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
