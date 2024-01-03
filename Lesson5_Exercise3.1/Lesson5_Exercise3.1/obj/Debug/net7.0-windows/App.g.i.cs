3e0114884bd57fd71fed2066b3e8857c5225ff4c
trungthanh324 <buithanh10112000@gmail.com>
2024-01-03 21:45:19 +0700

    AAA

--- Lesson5_Exercise3.1/Lesson5_Exercise3.1/obj/Debug/net7.0-windows/App.g.i.cs
+++ Lesson5_Exercise3.1/Lesson5_Exercise3.1/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Lesson5_Exercise3._1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Lesson5_Exercise3._1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             Lesson5_Exercise3._1.App app = new Lesson5_Exercise3._1.App();
             app.InitializeComponent();
